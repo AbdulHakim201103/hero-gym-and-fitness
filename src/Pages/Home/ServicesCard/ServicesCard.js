73204f410672192dabe9a862095677ca1bcc94fd
AbdulHakim201103 <abdulhakim201103@gmail.com>
2022-04-18 21:36:13 +0600

    service edit

--- src/Pages/Home/ServicesCard/ServicesCard.js
+++ src/Pages/Home/ServicesCard/ServicesCard.js
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 import "./ServicesCard.css";
 
 const ServicesCard = ({ service }) => {
-  const { name, img, description } = service;
+  const { name, img,price, description } = service;
   return (
     <div className="service">
       <img className="w-100" src={img} alt="" />
       <div className="service-info">
-        <h2>{name}</h2>
+        <h2 className="text-center">{name}</h2>
         <p>{description}</p>
+        <p className="text-success">Service Price: <span className="text-danger fs-1">{price}</span>/mo</p>
         <div className="d-flex justify-content-center align-content-center">
           <Link className="btn btn-danger my-2" to="/checkout">
             Book Now
